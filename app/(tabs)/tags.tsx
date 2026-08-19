15c97d1cb3261355322f9c5b01cac8a44df48ea9
Jannis Bögeholz <boegedev@gmail.com>
2026-08-19 14:59:40 +0200

    Umbau Tags

--- app/(tabs)/tags.tsx
+++
@@ -1,8 +0,0 @@
-/** Screen 11 — Tag-Verwaltung (Blatt `3f`). */
-import React from 'react';
-
-import { TagsScreen } from '../../src/screens/tags/TagsScreen';
-
-export default function TagsRoute() {
-  return <TagsScreen />;
-}
